31ebe1b620eb4efd52ee10421dd53948f2e6fa19
erick <erickgoncalves5@hotmail.com>
2023-06-05 11:19:17 -0300

    hello world

---
+++ index.js
@@ -0,0 +1,14 @@
+/** First hello world on Node.js */
+
+const express = require('express');
+const { readFile } = require('fs').promises;
+
+const app = express();
+
+app.get('/', async (request, response) => {
+    response.send( await readFile('./home.html', 'utf8'));
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app available on http://localhost:3000')
+})
